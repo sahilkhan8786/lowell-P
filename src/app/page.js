19562426ce74cc8c3772ce1485fb3f0ac464a4a8
Mr. WebWizard <142308196+sahilkhan8786@users.noreply.github.com>
2024-06-25 13:17:38 +0530

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,10 @@ import Header from '@/components/Header'
 import List from '@/components/List'
 import VideoPlayer from '@/components/VideoPlayer'
 import React from 'react'
+import image1 from '@/assets/final-1.jpg'
+import image2 from '@/assets/final-2.jpg'
+import image3 from '@/assets/final-3.jpg'
+import image4 from '@/assets/final-4.jpg'
 
 export default function Home() {
   return (
@@ -12,10 +16,10 @@ export default function Home() {
       <Details />
       <List />
       <h2 className='text-center text-4xl uppercase py-4 font-semibold'> All Videos</h2>
-      <VideoPlayer url={'https://player.vimeo.com/video/962918624'}/>
-      <VideoPlayer url={'https://player.vimeo.com/video/962930573'}/>
-      <VideoPlayer url={'https://player.vimeo.com/video/962930825'}/>
-      <VideoPlayer url={'https://player.vimeo.com/video/962930688'}/>
+      <VideoPlayer url={'https://player.vimeo.com/video/962918624'} title={'Voted for Border WALL'} thumbnail={image1} />
+      <VideoPlayer url={'https://player.vimeo.com/video/962930573'} title={'Puts Montana First'} thumbnail={image2} />
+      <VideoPlayer url={'https://player.vimeo.com/video/962930825'} title={'He’s working with Border Patrol to control Fentanyl in Montana'} thumbnail={image3} />
+      <VideoPlayer url={'https://player.vimeo.com/video/962930688'} title={'More and more lies, save MONTANA'} thumbnail={image4} />
       <Appeal />
     </>
   )
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,33 +1,29 @@
 'use client'
-import React, { Suspense, useState } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import VimeoPlayer from './VimeoPlayer'
-import thumb from '@/img.jpg'
-
-export default function VideoPlayer({ url, thumbnail }) {
+import { FaPlay } from "react-icons/fa";
+export default function VideoPlayer({ url, thumbnail, title }) {
     const [isPlaying, setIsPlaying] = useState(false);
     function playerhandler() {
         setIsPlaying(true);
     }
 
     return (
-        <secion className='flex flex-col gap-2 bg-white px-2 py-4 mb- w-full' >
-            <p className='text-center uppercase font-bold text-xl text-gray-500'>Video 1-TITLE OF VIDEO HERE</p>
+        <section className='flex flex-col gap-2 bg-white px-2 py-4 mb- w-full relative ' onClick={playerhandler}>
+            <div className='size-24 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                <span className='w-full h-full rounded-full text-white flex items-center justify-center border-2 cursor-pointer'>
+                    <FaPlay className='text-4xl pl-2 hover:scale-150 transition-all ' />
+                </span>
+            </div>
+            <p className='text-center uppercase font-bold text-xl text-gray-500'>{title}</p>
+            {isPlaying ? (
+                <VimeoPlayer url={url} />
+            ) : (
+                <Image src={thumbnail} priority className='w-full cursor-pointer ' alt='thumbnail' />
+            )}
+        </section>
 
-            {
-                isPlaying ? (
-                    <VimeoPlayer url={url} />
-                ) :
-                    (
-                        <div className='relative h-[500px]'>
-                            <Image onClick={playerhandler} src={'https://img.freepik.com/free-vector/cartoon-school-bus-children-with-flat-design_23-2147840786.jpg?size=626&ext=jpg'} priority fill className='absolute cursor-pointer' alt='thumbnail'></Image>
-                            <Image onClick={playerhandler} src={thumb} priority fill className='absolute cursor-pointer' alt='thumbnail'></Image>
-                        </div>
-                    )
-            }
-
-
-        </secion>
 
     )
 }
