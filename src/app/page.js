5e513f6c75ea208970cc45e24adc68c2d1b749db
Mr. WebWizard <142308196+sahilkhan8786@users.noreply.github.com>
2024-06-28 16:50:25 +0530

    Add files via upload
    added 2 more videos

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,8 @@ import image1 from '@/assets/final-1.jpg'
 import image2 from '@/assets/final-2.jpg'
 import image3 from '@/assets/final-3.jpg'
 import image4 from '@/assets/final-4.jpg'
+import image5 from '@/assets/final-5.jpg'
+import image6 from '@/assets/final-6.jpg'
 
 export default function Home() {
   return (
@@ -18,16 +20,22 @@ export default function Home() {
       <h2 className='text-center text-4xl uppercase py-4 font-semibold'> All Videos</h2>
       <VideoPlayer
         url={'https://player.vimeo.com/video/962918624'}
-        title={'Commercial 1 by Jon Tester'} thumbnail={image1} />
+        title={'Commercial 1 by Jon Tester and Washington'} thumbnail={image1} />
       <VideoPlayer
         url={'https://player.vimeo.com/video/962930573'}
-        title={'Commercial 2 by Jon Tester'} thumbnail={image2} />
+        title={'Commercial 2 by Jon Tester and Washington'} thumbnail={image2} />
       <VideoPlayer
         url={'https://player.vimeo.com/video/962930825'}
-        title={'Commercial 3 by Jon Tester'} thumbnail={image3} />
+        title={'Commercial 3 by Jon Tester and Washington'} thumbnail={image3} />
       <VideoPlayer
         url={'https://player.vimeo.com/video/962930688'}
-        title={'Commercial 4 by Jon Tester'} thumbnail={image4} />
+        title={'Commercial 4 by Jon Tester and Washington'} thumbnail={image4} />
+      <VideoPlayer
+        url={'https://player.vimeo.com/video/970544250'}
+        title={'Commercial 5 by Jon Tester and Washington'} thumbnail={image5} />
+      <VideoPlayer
+        url={'https://player.vimeo.com/video/970544004'}
+        title={'Commercial 6 by Jon Tester and Washington'} thumbnail={image6} />
       <Appeal />
     </>
   )
